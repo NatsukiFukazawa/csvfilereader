1bad7edc0bd70c1ead0b2f242d2fb6e527c50c43
NatsukiFukazawa <aiooksa929@gmail.com>
2020-06-19 17:25:11 +0900

    spa実装

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>csvfilereader</h1>
-    <!--<HelloWorld msg="vueの世界にようこそ！"> -->
-    <csvfilereader></csvfilereader>
+    <myheader></myheader>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+    <button class="btn btn-primary">test</button>
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-import csvfilereader from './components/csvfilereader.vue'
+import myheader from './components/myheader'
 
 export default {
   name: 'App',
   components: {
-    //HelloWorld,
-    csvfilereader
-  }
+    myheader
+  },
 }
 </script>
+
 
 <style>
 #app {
@@ -27,6 +29,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
